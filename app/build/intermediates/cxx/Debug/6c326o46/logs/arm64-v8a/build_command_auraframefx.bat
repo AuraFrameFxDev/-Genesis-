93d1a1cf20545826d6da07b329fd06dd7d4eb508
AuraKai <wehttam1989@gmail.com>
2025-08-08 19:34:31 -0600

    update

--- app/build/intermediates/cxx/Debug/6c326o46/logs/arm64-v8a/build_command_auraframefx.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Wehtt\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Wehtt\\Desktop\\Genesis\\app\\.cxx\\Debug\\6c326o46\\arm64-v8a" ^
-  auraframefx
